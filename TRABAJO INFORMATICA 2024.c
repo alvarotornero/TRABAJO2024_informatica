946673ac641d6a8c06526a3465c4c1df0f5a488d
marinaadh <marina.donalonso@alumnos.upm.es>
2024-05-21 10:25:09 +0200

    inicio

---
+++ TRABAJO INFORMATICA 2024.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <locale.h>
+
+typedef struct{
+	
+		int hora;
+		int NO2;
+		float PM25;
+		float PM10;
+		char cal_aire;
+
+}TMendezAlvaro;
+
+typedef struct{
+
+	int hora;
+	float T;
+	int HR;
+
+}TEscuelasAguirre;
+
+
+int main(){
+	
+	
+	
+	
+	return 0;
+}
